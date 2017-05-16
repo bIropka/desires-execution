6a1623f82f6713a531bd26080581390faff07f2c
bIropka <biropka@gmail.com>
2017-05-17 01:21:16 +0300

    Initial commit

---
+++ js/custom-scripts.js
@@ -0,0 +1,31 @@
+$(window).ready(function () {
+
+    if($(window).width() < 1281) {
+        $('.link-phone').insertAfter('.order-free');
+        $('.link-email').insertAfter('.link-phone');
+        $('.form-order').insertAfter('.you-will-know');
+    } else {
+        $('.link-phone').insertAfter('.logo');
+        $('.link-email').insertAfter('.link-phone');
+        $('.form-order').insertAfter('.order-free');
+    }
+
+    $(window).resize(function() {
+
+        if($(window).width() < 1281) {
+            $('.link-phone').insertAfter('.order-free');
+            $('.link-email').insertAfter('.link-phone');
+            $('.form-order').insertAfter('.you-will-know');
+        } else {
+            $('.link-phone').insertAfter('.logo');
+            $('.link-email').insertAfter('.link-phone');
+            $('.form-order').insertAfter('.order-free');
+        }
+
+    });
+
+    $('.order-free').click(function() {
+        $('html, body').animate({scrollTop: $('.form-order').offset().top - 50}, 600);
+    });
+
+});
